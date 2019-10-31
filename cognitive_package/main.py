fa5835c422b9229c9a23e0a41c34f171a3b43670
FeryET <farhoodetaati@ut.ac.ir>
2019-10-31 18:46:57 +0330

    adds todo

--- cognitive_package/main.py
+++ cognitive_package/main.py
@@ -1,6 +1,6 @@
 import model.cognitive_classifier_model
 
-
+# TODO: remove pymagnitude from the project.
 def main():
     mag_dir = "Resources/WordVectors/wiki-news-300d-1m-subword.magnitude"
     models_dir = "Resources/Pickles/"
