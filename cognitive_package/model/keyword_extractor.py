e7c37c0e86ea089d7f2781e6b529f97829ef558f
FeryET <farhoodetaati@ut.ac.ir>
2019-11-10 01:11:49 +0330

    adds keyword exctractor model

---
+++ cognitive_package/model/keyword_extractor.py
@@ -0,0 +1,16 @@
+from pke.unsupervised import PositionRank
+
+
+class KeyWordExtractor:
+    def __init__(self):
+        self.__extractor = PositionRank()
+
+    def get_keywords(self, text, num_of_keywords=10):
+        self.__extractor.load_document(
+            text, language='en', normalization='lemmatization')
+        self.__extractor.candidate_selection(maximum_word_number=3)
+        self.__extractor.candidate_weighting(window=10)
+        for keyphrase, score in self.__extractor.get_n_best(n=num_of_keywords,
+                                                redundancy_removal=False,
+                                                stemming=False):
+            yield keyphrase, score
